cd888f7b72c8e6909d54922f9ef2dac104b0fac6
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-01-11 06:02:40 +0100

    application cleaning

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,21 +1,17 @@
 import { takeEvery, takeLatest, call, put, select, delay, debounce, throttle } from 'redux-saga/effects';
 import { getExampleTasks } from './getExampleTasks';
-import { fetchExampleTasks, setTasks, selectTasks} from './tasksSlice';
+import { fetchExampleTasks, setTasks, selectTasks } from './tasksSlice';
 import { saveTasksInLocalStorage } from "./tasksLocalStorage"
 function* fetchExampleTasksHandler() {
     try {
-        yield delay(2000)
+        yield delay(1000)
         const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks({exampleTasks,state:"Loading"}));
-    
-        
+        yield put(setTasks(exampleTasks));
     } catch (error) {
         const er = yield put({ type: "error", error })
         console.log(er)
     }
 }
-console.log(fetchExampleTasksHandler())
-
 function* saveTasksInLocalStorageHandler() {
     const tasks = yield select(selectTasks);
     yield call(saveTasksInLocalStorage, tasks)
@@ -24,7 +20,7 @@ function* saveTasksInLocalStorageHandler() {
 
 export function* tasksSaga() {
     console.log("saga jest podłączona")
-    yield takeLatest( fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
     yield takeEvery("*", saveTasksInLocalStorageHandler)
 
 
