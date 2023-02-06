fbdbe96d8de81de2ffb6a44600738793270bb76c
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-02-06 18:53:47 +0100

    improvement of appearance

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -60,8 +60,7 @@ export const selectIsTasksEveryDone = state => selectTasks(state).every(({ done 
 export const getTaskById = (state, taskId) =>
     selectTasks(state).find(({ id }) => id === taskId);
     
- 
-export const selectTasksByQuery = (state, query) => {
+ export const selectTasksByQuery = (state, query) => {
     const tasks = selectTasks(state);
 
     if (!query || query.trim() === "") {
