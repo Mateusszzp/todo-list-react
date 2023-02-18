2999250749564c31f0ac0198ca6470b9ebcb6210
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-02-18 15:28:26 +0100

    logic setting in useReplaceQueryParameters

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,28 +1,22 @@
-import { useHistory, useLocation } from "react-router-dom";
 import searchQueryParamsName from "./searchQueryParamsName";
 import { useQueryParameters, useReplaceQueryParameters } from "../../queryParameters";
 import { Wrapper } from "./styled";
 import Input from "../../Input/styled"
 
 const Search = () => {
-	const location = useLocation();
-	const history = useHistory();
+
 	const query = useQueryParameters();
-
+	const replaceQueryParameters = useReplaceQueryParameters();
 	const onInputchange = ({ target }) => {
-		const searchParams = new URLSearchParams(location.search);
-
-		if (target.value.trim() === "" ? target.value : undefined) {
-
-			searchParams.delete(searchQueryParamsName);
-		} else {
-			searchParams.set(searchQueryParamsName, target.value)
-		}
-		history.push(`${location.pathname}?${searchParams.toString()}`);
+		replaceQueryParameters({
+			key: searchQueryParamsName,
+			value: target.value.trim() !== "" ? target.value : undefined,
+		})
 	};
 	return (
 		<Wrapper>
 			<Input
+			
 				placeholder="Filter zadań"
 				value={query || ""}
 				onChange={onInputchange} />
--- src/features/tasks/queryParameters.js
+++ src/features/tasks/queryParameters.js
@@ -1,25 +1,33 @@
-import { useEffect } from "react";
-import { useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import searchQueryParamsName from "./TasksPage/Search/searchQueryParamsName";
 
 
 
 export const useQueryParameters = () => {
-  const location = useLocation()
+  const location = useLocation();
   const loc = new URLSearchParams(location.search).get(searchQueryParamsName)
 
   return loc
 }
-export const useReplaceQueryParameters = (value, key) => {
+export const useReplaceQueryParameters = () => {
   const history = useHistory();
-  const location = useLocation()
+  const location = useLocation();
+
   const searchParams = new URLSearchParams(location.search);
 
- 
+  return ({value, key}) => {
+    if (value) {
 
-  return { value, key }
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key,value)
+    }
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  }
+  
 
-}
+};
+
+
 
 
