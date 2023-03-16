157034bd32f686db1d4ed57b00d2069052f2f6ab
Mateusszzp <mateusz.pasko.r@gmail.com>
2023-03-16 22:14:28 +0100

    deleting comments

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 export const theme = {
     colors:{
-        teal: "#0d6e79",//duży buuton
-        Laur: "#008000",//zilony mały button
-        milanoRed: "#BF0D0D",// czerwony mały button
-        white: "#ffffff",//tło białe
-        mercury: "#e7e5e5",//obramowanie inputa
-        alto: "#dadada"
+        teal: "#0d6e79",
+        Laur: "#008000",
+        milanoRed: "#BF0D0D",
+        white: "#ffffff",
+        mercury: "#e7e5e5",
+        alto: "#dadada",
     },
     breakpoints:{
         mobile:767,
